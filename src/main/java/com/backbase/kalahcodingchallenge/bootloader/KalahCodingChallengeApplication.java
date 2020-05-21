fc400fdde9ebc00e23a82ee582facb200a8865d1
Vikram Mishra <vikram.mishra420@gmail.com>
2020-05-21 11:34:55 +0200

    develop: initial commit with directory structure

---
+++ src/main/java/com/backbase/kalahcodingchallenge/bootloader/KalahCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.backbase.kalahcodingchallenge.bootloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalahCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backbase/kalahcodingchallenge/KalahCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backbase.kalahcodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalahCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
